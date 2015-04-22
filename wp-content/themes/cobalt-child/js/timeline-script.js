691371c9607c74420cf27b35ac3fa9d31fc7439c
Sebastian <sebasgd@gmail.com>
2015-04-22 17:13:55 -0500

    Navigation for the timeline.

--- wp-content/themes/cobalt-child/js/timeline-script.js
+++ wp-content/themes/cobalt-child/js/timeline-script.js
@@ -4,5 +4,13 @@ jQuery(document).ready( function () {
 			nextEl : '#bb-nav-next',
 			prevEl : '#bb-nav-prev'
 		});
+		jQuery("#bb-nav-first").on("click", function (e) {
+			e.preventDefault();
+			$("#bb-bookblock").bookblock("first");
+		});
+		jQuery("#bb-nav-last").on("click", function (e) {
+			e.preventDefault();
+			$("#bb-bookblock").bookblock("last");
+		});
 	}
 });
